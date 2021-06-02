b86a187db36173be7dcd57ba97c2bc876237d990
xiaoguokf <1742137309@qq.com>
2021-06-02 12:45:09 +0800

    开启sql事务管理

--- src/main/java/org/sanyuankexie/attendance/GctaAttendanceSystemApplication.java
+++ src/main/java/org/sanyuankexie/attendance/GctaAttendanceSystemApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@MapperScan("org.sanyuankexie.attendance.mapper")
-@EnableScheduling
+@MapperScan("org.sanyuankexie.attendance.mapper") //扫描mapper
+@EnableScheduling   //开启计划任务
+@EnableTransactionManagement  //开启sql事务
 public class GctaAttendanceSystemApplication {
 
     public static void main(String[] args) {
