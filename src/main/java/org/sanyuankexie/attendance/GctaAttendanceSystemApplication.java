d62bdbfce3debd7aaf5b75ae43932d0fc238b961
xiaoguokf <64497205+xiaoguokf@users.noreply.github.com>
2021-06-03 11:24:45 +0800

    修改了一些pom (#1) -> 解决了一些奇奇怪怪的问题
    * readme
    
    1.使用mybatis-plus编写sql
    2.使用spring-boot自带的定时任务
    3.使用yml风格做配置
    
    * 开启sql事务管理

--- src/main/java/org/sanyuankexie/attendance/GctaAttendanceSystemApplication.java
+++ src/main/java/org/sanyuankexie/attendance/GctaAttendanceSystemApplication.java
@@ -1,9 +1,15 @@
 package org.sanyuankexie.attendance;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("org.sanyuankexie.attendance.mapper") //扫描mapper
+@EnableScheduling   //开启计划任务
+@EnableTransactionManagement  //开启sql事务
 public class GctaAttendanceSystemApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/sanyuankexie/attendance/common/DTO/RecordDTO.java
+++ src/main/java/org/sanyuankexie/attendance/common/DTO/RecordDTO.java
@@ -1,5 +1,9 @@
 package org.sanyuankexie.attendance.common.DTO;
 
+
+import lombok.Data;
+
+@Data
 public class RecordDTO{
     private Long userId;
     private String userName;
@@ -10,67 +14,5 @@ public class RecordDTO{
     private Object status; // 1 -> Online, 0 -> Offline, -1 -> Be reported
     private Object accumulatedTime;
 
-    public Object getAccumulatedTime() {
-        return accumulatedTime;
-    }
 
-    public void setAccumulatedTime(Object accumulatedTime) {
-        this.accumulatedTime = accumulatedTime;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserDept() {
-        return userDept;
-    }
-
-    public void setUserDept(String userDept) {
-        this.userDept = userDept;
-    }
-
-    public String getUserLocation() {
-        return userLocation;
-    }
-
-    public void setUserLocation(String userLocation) {
-        this.userLocation = userLocation;
-    }
-
-    public Object getStart() {
-        return start;
-    }
-
-    public void setStart(Object start) {
-        this.start = start;
-    }
-
-    public Object getEnd() {
-        return end;
-    }
-
-    public void setEnd(Object end) {
-        this.end = end;
-    }
-
-    public Object getStatus() {
-        return status;
-    }
-
-    public void setStatus(Object status) {
-        this.status = status;
-    }
 }
